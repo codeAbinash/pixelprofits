c004261945f906796ddb4c2b6400970c5fd7cb10
Abinash Karmakar <codeAbinash@gmail.com>
2023-11-27 15:36:07 +0530

    first commit

---
+++ screens/lib.tsx
@@ -0,0 +1,3 @@
+import { Alert } from 'react-native'
+
+export function networkError(err: any, cb: Function) {}
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./App.{js,jsx,ts,tsx}', './screens/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        lightBg: '#f0f0f0',
+        inputBorder: '#E0E0E0',
+        red: '#FF0000',
+        white: '#FFFFFF',
+      },
+    },
+  },
+  plugins: [],
+}
